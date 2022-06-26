953abe57ab10af51bb8ed178fddae41fca8ef614
Dmitrij Fedorenko <job@fedorenko-d.ru>
2022-06-26 13:23:36 +0300

    Fix eslint warnings

--- src/cmd.js
+++ src/cmd.js
@@ -1,47 +1,46 @@
-//#!/usr/bin/env node
+// #!/usr/bin/env node
 
 import {
-    Octokit,
-} from "@octokit/core"
+  Octokit
+} from '@octokit/core'
 import {
-    paginateRest,
-} from "@octokit/plugin-paginate-rest"
-
+  paginateRest
+} from '@octokit/plugin-paginate-rest'
 
 import {
-    PackageCleaner,
-} from "./cleaner.js"
+  PackageCleaner
+} from './cleaner.js'
 
 const PaginableOctokit = Octokit.plugin(paginateRest)
 
 const CONCURRENCY_SIZE = process.env.CONCURRENCY_SIZE !== undefined
-      ? parseInt(process.env.CONCURRENCY_SIZE)
-      : undefined
+  ? parseInt(process.env.CONCURRENCY_SIZE)
+  : undefined
 
 const {
-    GH_TOKEN,
-    OWNER,
-    OWNER_TYPE,
-    PACKAGE_NAME,
+  GH_TOKEN,
+  OWNER,
+  OWNER_TYPE,
+  PACKAGE_NAME
 } = process.env
 
 console.log({
-    OWNER,
-    OWNER_TYPE,
-    PACKAGE_NAME,
-    CONCURRENCY_SIZE,
+  OWNER,
+  OWNER_TYPE,
+  PACKAGE_NAME,
+  CONCURRENCY_SIZE
 })
 
 const octokit = new PaginableOctokit({
-    auth: GH_TOKEN,
+  auth: GH_TOKEN
 })
 
 const cleaner = new PackageCleaner({
-    octokit,
-    owner: OWNER,
-    ownerType: OWNER_TYPE,
-    packageName: PACKAGE_NAME,
-    concurrencySize: CONCURRENCY_SIZE,
+  octokit,
+  owner: OWNER,
+  ownerType: OWNER_TYPE,
+  packageName: PACKAGE_NAME,
+  concurrencySize: CONCURRENCY_SIZE
 })
 
 await cleaner.cleanPackage()
